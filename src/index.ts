ffa8dc541b8699c8a593f7147e5349b10c755816
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-24 18:56:32 -0400

    Lint project files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 // A completely unneeded function to fill in here
 function add(left: number, right: number): number {
-  return left + right;
+    return left + right;
 }
 
 const num1 = 5;
 const num2 = 37;
 const sum = add(num1, num2);
 
+// eslint-disable-next-line no-console
 console.log(`${num1} + ${num2} = ${sum}`);
