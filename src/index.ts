231f68845293181b46c45c1d3a00520f17dd40e5
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-24 18:27:26 -0400

    Add basic entry point

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// A completely unneeded function to fill in here
+function add(left: number, right: number): number {
+  return left + right;
+}
+
+const num1 = 5;
+const num2 = 37;
+const sum = add(num1, num2);
+
+console.log(`${num1} + ${num2} = ${sum}`);
