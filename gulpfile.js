45c14494a8bec4778cc8274140a320fe37fcd5ed
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-24 20:09:02 -0400

    Add basic gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// Imports
+const del = require('del');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const ts = require('gulp-typescript');
+
+// TypeScript project setup
+const tsProject = ts.createProject('tsconfig.app.json');
+
+// Tasks
+function build() {
+    return tsProject.src().pipe(babel()).pipe(terser()).pipe(gulp.dest('dist'));
+}
+
+function clean() {
+    return del(['dist']);
+}
+
+exports.default = gulp.series(clean, build);
